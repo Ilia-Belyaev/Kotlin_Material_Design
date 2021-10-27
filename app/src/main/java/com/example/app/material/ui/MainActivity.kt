66ee559efba4fb360b0d1a541fe6963c2b22069b
Ilia Belyaev <ilya.belyaev.95@list.ru>
2021-10-27 19:34:50 +0300

    2 Дз.

--- app/src/main/java/com/example/app/material/ui/MainActivity.kt
+++ app/src/main/java/com/example/app/material/ui/MainActivity.kt
@@ -9,11 +9,22 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(ThemeHolder.theme)
         setContentView(R.layout.main_activity)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, PictureOfTheDayFragment.newInstance())
                 .commitNow()
+
         }
     }
+
+    fun changeTheme(){
+        ThemeHolder.theme = R.style.My_theme_v2
+
+        recreate()
+    }
+}
+object ThemeHolder{
+    var theme = R.style.My_theme
 }
